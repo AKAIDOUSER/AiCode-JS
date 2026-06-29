9c256cfb7966bd0045749d6524f499d65401c970
AKAIDOUSER <enzo.silva.soares2@gmail.com>
2026-06-29 14:59:36 -0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,1 @@
-javascript:(function(){const s=document.createElement('script');s.src='https://raw.githubusercontent.com/AKAIDOUSER/AiCode-JS/refs/heads/main/License.js';document.head.appendChild(s)})();
+javascript:(function(){const s=document.createElement('script');s.src='https://cdn.jsdelivr.net/gh/AKAIDOUSER/AiCode-JS@main/License.js';document.head.appendChild(s)})();
