6051de350c8c54cf40392c1fc169d694e32fdb59
AKAIDOUSER <enzo.silva.soares2@gmail.com>
2026-06-29 12:58:59 -0300

    Update WhiteList.js

--- WhiteList.js
+++ WhiteList.js
@@ -1,1 +1,5 @@
-
+[
+    { "username": "Kaliu", "password": "86271415", "patent": "Administrator" },
+    { "username": "user1", "password": "pass1", "patent": "Premium" },
+    { "username": "user2", "password": "pass2", "patent": "Basic" }
+]
